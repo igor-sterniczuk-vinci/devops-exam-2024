dea1129bd1667b71400564820e6ca2c67f96d30b
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2024-01-15 17:09:40 +0100

    style: prettier and lint fix

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -11,12 +11,12 @@ test('should return true if the gamertag has 8 characters but without a special 
     expect(result).toBe(false);
 });
 
-test("should return true if the gamertag has 8 characters and a special character", () => {
-    const result = isValid("abc&defg");
+test('should return true if the gamertag has 8 characters and a special character', () => {
+    const result = isValid('abc&defg');
     expect(result).toBe(true);
 });
 
-test("should return true if the gamertag has more than 8 characters and a special character", () => {
-    const result = isValid("abc&defghi");
+test('should return true if the gamertag has more than 8 characters and a special character', () => {
+    const result = isValid('abc&defghi');
     expect(result).toBe(true);
 });
--- src/services/validations.js
+++ src/services/validations.js
@@ -1,6 +1,6 @@
 // TODO: Refactor validation function to follow the defined validations rules
 const validation = (gamertag) => {
-    const length = gamertag.length >= 8
+    const length = gamertag.length >= 8;
     const hasSpecialCharacter = /[&$!è§à_]/.test(gamertag);
     return length && hasSpecialCharacter;
 };
