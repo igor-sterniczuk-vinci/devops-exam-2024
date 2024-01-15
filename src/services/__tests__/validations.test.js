2225a0bd6f55ba1598255f0e0f937286fe9becfa
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2024-01-15 17:16:27 +0100

    test: update test to take care of the number

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -11,15 +11,11 @@ test('should return true if the gamertag has 8 characters but without a special 
     expect(result).toBe(false);
 });
 
-test('should return true if the gamertag has 8 characters and a special character', () => {
-    const result = isValid('abc&defg');
+test("should return true if the gamertag has 8 characters, a special character, and a number", () => {
+    const result = isValid("abc&d1ef");
     expect(result).toBe(true);
 });
 
-test('should return true if the gamertag has more than 8 characters and a special character', () => {
-    const result = isValid('abc&defghi');
-    expect(result).toBe(true);
-});
 
 test("should return true if the gamertag has more than 8 characters, a special character, and a number", () => {
     const result = isValid("abc&d1efghi");
