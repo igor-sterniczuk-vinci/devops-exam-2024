c3a891bb4cf35f0d177a5e077c3cc7df27867587
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2024-01-15 17:20:31 +0100

    refactor: refactor function validation to handle if it has a number

--- src/services/validations.js
+++ src/services/validations.js
@@ -2,7 +2,8 @@
 const validation = (gamertag) => {
     const length = gamertag.length >= 8;
     const hasSpecialCharacter = /[&$!è§à_]/.test(gamertag);
-    return length && hasSpecialCharacter;
+    const hasANumber = /[0-9]/.test(gamertag);
+    return length && hasSpecialCharacter &&  hasANumber;
 };
 
 exports.isValid = validation;
