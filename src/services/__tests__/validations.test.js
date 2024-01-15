c0ba74f453c1b9c9c1374270073378c2f4a74c5d
igor-sterniczuk-vinci <130989091+igor-sterniczuk-vinci@users.noreply.github.com>
2024-01-15 16:39:15 +0100

    test: add test to check 8 characters

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,9 +1,7 @@
 // TODO: Create tests suite for validation function
 const { isValid } = require('../validations');
 
-describe('validations tests suites - isValid', () => {
-    test('should return true or false as the function is defined', () => {
-        const result = isValid();
-        expect(result).toBeDefined();
-    });
+test("should return false if the gamertag does not have 8 characters", () => {
+    const result = validation("aaaaaaa");
+    expect(result).toBe(false);
 });
